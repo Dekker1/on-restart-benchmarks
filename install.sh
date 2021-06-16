69f55d8f982698df4c0cc1a8f460e77b29a69373
Jip J. Dekker <jip@dekker.one>
2021-06-16 13:15:25 +1000

    Add correct version of the MiniZinc compiler

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ module load flex
 mkdir -p software/{build,install}
 
 # Build Chuffed
-for dir in "chuffed" "gecode" "gecode_on_record" "gecode_on_replay"
+for dir in "chuffed" "gecode" "gecode_on_record" "gecode_on_replay" "minizinc"
 do
 	cmake -S software/${dir} -B software/build/${dir} -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=`pwd`/software/install/${dir}
 	cmake --build software/build/${dir} --config Release --target install -- -j4
--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ timeout_sec=120
 record_timeout_sec=240
 runs=10
 
-module load MiniZinc/2.5.5
+export PATH=`pwd`/software/install/minizinc/bin:$PATH
